4d8ac2a0f868511893806d22d458ad3499706104
gh0st <127751354+8gh8@users.noreply.github.com>
2025-01-19 22:27:43 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createGame } from "./game.js";
+import { connectWallet } from "./wallet.js";
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const { account } = await connectWallet();
+    if (account) {
+        console.log(`Player account: ${account}`);
+        createGame('game-container');
+    }
+});
